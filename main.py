0c49eff0fd8870feaab3542876a0734cb3d5021c
Divyansh Purohit <72261138+dp-0509@users.noreply.github.com>
2023-06-19 15:18:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+def levenshtein_distance(str1, str2):
+    m, n = len(str1), len(str2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        dp[i][0] = i
+
+    for j in range(n + 1):
+        dp[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+
+    return dp[m][n]
+
+string_a = "armbnupz45ms"
+string_b = "26i7eglbu7a8"
+
+distance = levenshtein_distance(string_a, string_b)
+print(distance)
